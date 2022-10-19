b3cd7f15526b6e35de2a36dc5bf15653e98f2607
Ever Ruiz Diaz <ruizdiaz.oe@gmail.com>
2022-10-19 22:35:05 +0200

    Added GraphQL integration

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+from strawberry.asgi import GraphQL
+import strawberry
 import uvicorn
 from fastapi import (Depends, FastAPI, HTTPException, Request)
 from fastapi.middleware.cors import CORSMiddleware
+from src.blog import Query
 
 
+# APP
 app = FastAPI(debug=True)
 origins = ['*']
 
 
+# GRAPHQL
+schema = strawberry.Schema(query=Query)
+graphql_app = GraphQL(schema)
+app.add_route("/graphql", graphql_app)
+app.add_websocket_route("/graphql", graphql_app)
+
+
+# HOME
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World from FastAPI, Svelte, GraphQL and RedisDB"}
 
 
-@app.get("/about")
-async def root():
-    return {"name": "Ever"}
-
-
+# CORS
 app = CORSMiddleware(
     app=app,
     allow_origins=origins,
@@ -25,5 +33,6 @@ app = CORSMiddleware(
     allow_headers=["*"],
 )
 
+# INIT
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
